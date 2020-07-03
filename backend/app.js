4bac6955832727b21d3ef7bf8610f63fbabe8aaa
Erdinc <Erdinc@gklfmg.com>
2020-07-03 08:59:59 +0200

    getting place by user id

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,6 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placesRoutes);
+app.use('/api/places', placesRoutes); // => /api/places...
 
 app.listen(5000);
